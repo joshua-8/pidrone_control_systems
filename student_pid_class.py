e98b987ba01006e6ba0e23ea46d700c422809c8b
joshua-8 <joshua_phelps@brown.edu>
2023-05-16 01:42:02 -0400

    tune

--- student_pid_class.py
+++ student_pid_class.py
@@ -28,9 +28,9 @@ class PID:
         :param k: The offset constant that will be added to the sum of the P, I, and D control terms
         """
 
-        self._p = 2.0
+        self._p = 6.0
         self._i = 0.5
-        self._d = 0.25
+        self._d = 0.5
         self._k = k # not used
 
         self._lowLimit = 1100
