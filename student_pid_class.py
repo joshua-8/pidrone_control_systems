c0ca4a1e59479792ca0691c3a08802ebb9a5fcd9
joshua-8 <59814881+joshua-8@users.noreply.github.com>
2023-09-20 20:28:02 -0400

    restore old, tune p

--- student_pid_class.py
+++ student_pid_class.py
@@ -29,7 +29,7 @@ class PID:
         :param k: The offset constant that will be added to the sum of the P, I, and D control terms
         """
 
-        self._p = 1.5
+        self._p = 1
         self._i = 0.5
         self._d = 1
         self._k = k  # not used
